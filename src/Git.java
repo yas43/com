8d6ecebe61f1ba002fc5a8707c56a59e28b81f7d
yaser <y.r.k.r.2011@gmail.com>
2024-02-09 13:32:23 +0100

    thirdcommit

--- src/Git.java
+++ src/Git.java
@@ -2,5 +2,6 @@ public class Git {
     public static void main(String[] args) {
         String name1 = "firstcommit";
         String NAME2 = "secondcommit";
+        String name3 = "thirdcommit";
     }
 }
